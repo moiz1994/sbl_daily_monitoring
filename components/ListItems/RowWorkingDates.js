ce27bd85e7b6186477ee0b306f34639ec2ed7b1a
moiz1994 <moizwaseem1994@gmail.com>
2023-12-27 13:30:43 +0500

    Working Date Complete

--- components/ListItems/RowWorkingDates.js
+++ components/ListItems/RowWorkingDates.js
@@ -2,7 +2,40 @@ import { StyleSheet, Text, View } from "react-native";
 import Card from "../UI/Card";
 import { Colors } from "../../constants/Colors";
 
+function parseWorkingDate(dateString) {
+    const months = {
+        JAN: 0, FEB: 1, MAR: 2, APR: 3, MAY: 4, JUN: 5,
+        JUL: 6, AUG: 7, SEP: 8, OCT: 9, NOV: 10, DEC: 11
+    };
+
+    const dateParts = dateString.split('-');
+    const day = parseInt(dateParts[0], 10);
+    const month = months[dateParts[1]];
+    const year = 2000 + parseInt(dateParts[2], 10); // Assuming 2-digit year
+
+    return new Date(year, month, day);
+}
+
 function RowWorkingDates({workingDataItem}) {
+    // Convert WORKING_DATE string to a Date object
+    const workingDate = parseWorkingDate(workingDataItem.WORKING_DATE);
+    //const workingDate = new Date("26-DEC-23");
+    //console.log("Working Date: ", workingDate);
+
+    // Get the current date
+    const currentDate = new Date();
+    currentDate.setUTCHours(0, 0, 0, 0);
+
+    const localCurrentDate = new Date(Date.UTC(
+        currentDate.getUTCFullYear(),
+        currentDate.getUTCMonth(),
+        currentDate.getUTCDate()
+    ));
+
+
+    // Check if WORKING_DATE is less than the current date
+    const isPastDate = workingDate < currentDate;
+
     return ( 
         <View style={styles.root}>
             <Card style={styles.card}>
@@ -16,7 +49,7 @@ function RowWorkingDates({workingDataItem}) {
                 </View>
                 <View style={styles.container}>
                     <Text style={styles.heading}>Working Date:</Text>
-                    <Text style={styles.value}>{workingDataItem.WORKING_DATE}</Text>
+                    <Text style={[isPastDate ? styles.value : styles.pastDateText]}>{workingDataItem.WORKING_DATE}</Text>
                 </View>
             </Card>
         </View>
@@ -41,7 +74,12 @@ const styles = StyleSheet.create({
     },
     value: {
         color: Colors.gray700,
-    }
+    },
+    pastDateText: {
+        backgroundColor: Colors.red500,
+        color: 'white',
+        paddingHorizontal: 4,
+    },
 });
 
 export default RowWorkingDates;
